2bef2454adce7e29ff2f69bf4d62512bd3d48f9d
Nils Matic <matic1909@gmail.com>
2020-11-02 21:38:28 +0100

    created basic layout for the home page

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import styled from "styled-components";
 
-const HomePage = () => {
-  <div className="homepage">
+const HomePageStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 80px;
+
+  .directory-menu {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  .menu-item {
+    min-width: 30%;
+    height: 240px;
+    flex: 1 1 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid black;
+    margin: 0 7.5px 15px;
+
+    &:first-child {
+      margin-right: 7.5px;
+    }
+
+    &:last-child {
+      margin-left: 7.5px;
+    }
+
+    .content {
+      height: 90px;
+      padding: 0 25px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      border: 1px solid black;
+
+      .title {
+        font-weight: bold;
+        margin-bottom: 6px;
+        font-size: 22px;
+        color: #4a4a4a;
+      }
+
+      .subtitle {
+        font-weight: lighter;
+        font-size: 16px;
+      }
+    }
+  }
+`;
+
+const HomePage = () => (
+  <HomePageStyles className="homepage">
     <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
@@ -34,7 +90,7 @@ const HomePage = () => {
         </div>
       </div>
     </div>
-  </div>;
-};
+  </HomePageStyles>
+);
 
 export default HomePage;
